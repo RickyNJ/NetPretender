de8c09558334de32477bbfe8f731959c25f0b586
Ricky Nijsten <rickynijsten@gmail.com>
2025-07-18 14:05:15 +0200

    updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-current flow:
-
-
-
-for each command in commands.yaml
-first word in 
+Network device mocking
+This is the application that will be running inside a docker container you can ssh into
+The following features will be (hopefully) implemented:
+    - Yaml file parsing to store mocks in code
+    - Custom response file to keep longer responses out of the yaml
+    - Variables in commands
+    - Variables in output
+    - Output customization (delays, and outputting in multiple parts)
+    - Internal State of a machine
+    - Referencing to internal state in output
+    - Conditional outputs based on varialbes and internal state
--- src/main/java/com/rickynj/Main.java
+++ src/main/java/com/rickynj/Main.java
@@ -2,18 +2,23 @@ package com.rickynj;
 
 
 import com.rickynj.commands.CommandNode;
-import com.rickynj.responses.BasicResponse;
 import com.rickynj.responses.MultipartResponse;
 import com.rickynj.responses.Response;
-import com.rickynj.responses.ResponseBase;
 
+
+/**
+ * flow will be:
+ * new parser -> yaml parser for new (snakeyaml)
+ * parser.parse() -> new Device();
+ *
+ * new REPL(device);
+ *
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         Response r = new MultipartResponse();
-
         CommandNode c = new CommandNode();
-
         c.setResponse(r);
     }
 }
