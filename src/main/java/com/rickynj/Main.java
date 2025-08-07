445e873f9d76587467bb4db33ba11b9b1bec3356
RickyNJ <rickynijsten@gmail.com>
2025-08-07 16:50:15 +0200

    EOD save

--- src/main/java/com/rickynj/Main.java
+++ src/main/java/com/rickynj/Main.java
@@ -5,35 +5,25 @@ import com.rickynj.parser.YAMLParser;
 import com.rickynj.repl.Repl;
 import com.rickynj.repository.valkey.ValkeyClient;
 
-
-
 public class Main {
     public static void main(String[] args) {
         String valkeyConfigFile = "src/main/resources/redisson.yaml";
 //        String valkeyConfigFile = "/opt/configs/redisson.yaml";
-
         String commandFilesPath =  "src/main/resources/commands.yml";
 //        String commandFilesPath =  "/opt/configs/commands.yml";
 
-        DeviceManager deviceManager = getDeviceManager(valkeyConfigFile, commandFilesPath);
-
-        Repl repl = new Repl(deviceManager.getDeviceByPort(22));
-//        Repl repl = new Repl(deviceManager.getDeviceByPort(Integer.parseInt(args[0])));
-        repl.start();
-    }
-
-    private static DeviceManager getDeviceManager(String valkeyConfigFile, String commandFilesPath) {
         ValkeyClient client = new ValkeyClient(valkeyConfigFile, commandFilesPath);
-        DeviceManager deviceManager;
-
         // TODO: make caching optional.
-        deviceManager = client.getCachedDeviceManagerIfExists(commandFilesPath);
+        DeviceManager deviceManager = client.getCachedDeviceManagerIfExists(commandFilesPath);
         if (deviceManager == null) {
             deviceManager = new DeviceManager();
             YAMLParser parser = new YAMLParser(deviceManager, commandFilesPath);
             parser.parse();
             client.cacheNewDeviceManager(commandFilesPath, deviceManager);
         }
-        return deviceManager;
+
+        Repl repl  = new Repl(deviceManager.getDeviceByPort(22));
+//        Repl repl = new Repl(deviceManager.getDeviceByPort(Integer.parseInt(args[0])));
+        repl.start();
     }
 }
--- src/main/java/com/rickynj/repl/Repl.java
+++ src/main/java/com/rickynj/repl/Repl.java
@@ -4,16 +4,23 @@ package com.rickynj.repl;
 import com.rickynj.device.Device;
 import com.rickynj.domain.CommandContext;
 import com.rickynj.exception.CommandNotMockedException;
+import com.rickynj.repository.valkey.ValkeyClient;
 
 import java.util.Scanner;
 
 public class Repl {
+    private ValkeyClient valkeyClient;
     private Device device;
 
     // Each command check if first word appears as val in any root
     public Repl(Device device) {
         this.device = device;
     }
+    public Repl(Device device, ValkeyClient valkeyClient) {
+        this.device = device;
+        this.valkeyClient = valkeyClient;
+    }
+
     public void start() {
         Scanner scanner  = new Scanner(System.in);
         while (true) {
